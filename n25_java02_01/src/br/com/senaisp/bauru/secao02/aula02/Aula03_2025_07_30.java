b681d97559eae1fab5e22be4c0667775af2c4b9a
Rick <ricketttohs@gmail.com>
2025-07-30 21:53:24 -0300

    65001

---
+++ n25_java02_01/src/br/com/senaisp/bauru/secao02/aula02/Aula03_2025_07_30.java
@@ -0,0 +1,16 @@
+package br.com.senaisp.bauru.secao02.aula02;
+
+public class Aula03_2025_07_30 {
+
+	public static void main(String[] args) {
+		System.out.println("⬜⬜████⬜⬜");
+		System.out.println("⬜█⬜⬜⬜⬜█⬜");
+		System.out.println("█⬜█⬜⬜█⬜█");
+		System.out.println("█⬜⬜⬜⬜⬜⬜█");
+		System.out.println("█⬜█⬜⬜█⬜█");
+		System.out.println("█⬜⬜██⬜⬜█");
+		System.out.println("⬜█⬜⬜⬜⬜█⬜");
+		System.out.println("⬜⬜████⬜⬜");
+	}
+
+}
